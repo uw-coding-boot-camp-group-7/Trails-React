f32fdabee776d4f90a8057ef35eb658cf9e244de
Mario Rodriguez <mrodrigu@northseattle.edu>
2019-05-11 11:14:46 -0700

    update

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = process.env.port || 8080;
+const PORT = process.env.port || 5000;
 
 
 // Define middleware here
