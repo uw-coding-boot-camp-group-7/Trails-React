b5a0fccd3cf5241fa738bf754f4611cf7d2ceadd
Josh <ejosh.manuel@gmail.com>
2019-05-14 01:56:46 -0700

    Landing page added

---
+++ trails/src/components/Landing/index.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import Parallax from "../Parallax";
+import Slide from "../Slide";
+import Wrapper from "../Wrapper";
+import TrailCard from "../TrailCard";
+import Footer from "../Footer";
+import examples from "../../default.json";
+
+
+class Landing extends Component {
+
+  state = {
+    examples
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        {/* <Parallax /> */}
+        <Slide />
+        <Wrapper>
+          {this.state.examples.map(examples => (
+            <TrailCard
+              id={examples.id}
+              key={examples.id}
+              name={examples.name}
+              image={examples.image}
+              location={examples.location}
+            />
+          ))}
+        </Wrapper>
+        <Footer />
+      </React.Fragment>
+    );
+  }
+}
+
+export default Landing;
