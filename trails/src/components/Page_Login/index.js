382f1f4ec6b14da4621f93b2cb1f8c6fef63869e
Josh <ejosh.manuel@gmail.com>
2019-05-14 04:43:29 -0700

    style added

--- trails/src/components/Page_Login/index.js
+++ trails/src/components/Page_Login/index.js
@@ -6,40 +6,38 @@ import Footer from "../Footer";
 
 class Login extends Component {
 
-  
+
   render() {
     return (
       <React.Fragment>
         <Parallax />
-        <Wrapper>
-          <div>
-            <h2>Sign Up Form</h2>
+        <div className="console-back">
+          <div className="console">
             <form>
               <div className="form-group">
-                  <label for="exampleInputEmail1">Email address</label>
-                  <input 
-                  type="email" 
-                  className="form-control" 
-                  id="email-input" 
-                  placeholder="Email" 
-                  />
+                <label for="exampleInputEmail1"></label>
+                <input  
+                type="email" 
+                className="form-control" 
+                id="email-input" 
+                placeholder="Email" 
+                />
               </div>
               <div className="form-group">
-                  <label for="exampleInputPassword1">Password</label>
-                  <input 
-                  type="password" 
-                  className="form-control" 
-                  id="password-input" 
-                  placeholder="Password" 
-                  />
+                <label for="exampleInputPassword1"></label>
+                <input 
+                type="password" 
+                className="form-control" 
+                id="password-input" 
+                placeholder="Password" 
+                />
               </div>
-              <button type="submit" className="btn btn-default">Sign Up</button>
-              </form>
-              <br />
-              <p>Or log in <a href="/login">here</a></p>
-            </div>
-          </Wrapper>
-        <Footer />
+              <button type="submit" className="btn btn-default login-btn">Log in</button>
+            </form>
+            <p style={{float:"right"}}>Or create an account <a href="/login">here</a></p>
+          </div>
+        </div>
+        {/* <Footer /> */}
       </React.Fragment>
     );
   }
