92de6687f30711f5800806bff27dbdbda87b6a9b
Josh <ejosh.manuel@gmail.com>
2019-05-14 13:16:43 -0700

    Nav style updated

--- trails/src/components/Nav/index.js
+++ trails/src/components/Nav/index.js
@@ -10,7 +10,7 @@ function Navbar() {
             <ul>
                 <li><a id="login" href="/info">Trail info</a></li>  
                 <li><a id="login" href="/passport">Passport</a></li>  
-                <li><a id="signup" href="/login" style={{color: "#f7a434"}}>Log in</a></li>  
+                <li><a id="signup" href="/login">Log in</a></li>  
             </ul>
         </nav>
 
