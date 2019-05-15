c29e5aae1cc8f777dd5cef7efc711ac005fff6b2
Josh <ejosh.manuel@gmail.com>
2019-05-15 15:52:37 -0700

    Slide structure updated

--- trails/src/components/SlideHeader/index.js
+++ trails/src/components/SlideHeader/index.js
@@ -14,46 +14,6 @@ const styles = {
 
 class SlideHeader extends Component {
 
-    // state = {
-    //     example,
-    // }
-    
-    // // Run function when page loads
-    // componentDidMount() {
-    //     this.changeImg();
-    // }
-    
-    // // Change Image
-    // changeImg = () => {
-        
-    //     let images = [];
-        
-    //     images[0] = "http://lorempixel.com/400/200/animals";
-    //     images[1] = "http://lorempixel.com/400/200/sports";
-    //     images[2] = "http://lorempixel.com/400/200/food";
-    //     images[3] = "http://lorempixel.com/400/200/people";
-        
-    //     // let i = Math.floor(Math.random() * example.length); // Start Point
-    //     let i = Math.floor(Math.random() * images.length); // Start Point
-        
-    //     // const nextSlide = "../../images/hero.jpg";
-    //     const nextSlide = images[i];
-
-    //     document.querySelector(".slide").style.background = `url(${nextSlide}) no-repeat 50% 50%`;
-
-    //     // Check If Index Is Under Max
-    //     if(i < example.length - 1){
-    //       // Add 1 to Index
-    //       i++; 
-    //     } else { 
-    //       // Reset Back To O
-    //       i = 0;
-    //     }
-        
-    //     // Run function every x seconds
-    //     let interval = setTimeout('changeImg()', 3000);
-    // };
-
     render() {
         return (
             <div style={styles.container}>
