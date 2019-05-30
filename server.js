2ff3a3b04682f2a559d728d22b3946e918f9ec40
Mario Rodriguez <mrodrigu@northseattle.edu>
2019-05-29 20:31:44 -0700

    update: removed passport from client

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 require("dotenv").config();
 const express = require('express');
 const path = require('path');
-const routes = require ('./routes');
 const mongoose = require('mongoose');
+const routes = require ('./routes');
 const app = express();
 const PORT = process.env.PORT || 3001;
 const uristring = process.env.MONGODB_URI ||"mongodb://localhost/trails";
